5028a7539943b796188f131e20fd1f7becefa146
suleyman-yilmaz <suleymanymz50@gmail.com>
2024-12-05 11:40:42 +0300

    remove all files
    tüm dosyalar silindi

--- laravel-icerik/resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="py-6 px-6 text-center">
-    <p class="mb-0 fs-4">Developed by <a href="https://suleyman-yilmaz-portfolio.vercel.app/" target="_blank"
-            class="pe-1 text-primary text-decoration-underline">Süleyman Yılmaz</a>
-    </p>
-    <p class="mb-0 fs-4">Design by <a href="https://www.wrappixel.com/" target="_blank"
-            class="pe-1 text-primary text-decoration-underline">Wrappixel.com</a>
-    </p>
-</div>
