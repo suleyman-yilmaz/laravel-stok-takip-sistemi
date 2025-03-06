a664723e0c954251819e55de3d62b33becc9a059
Süleyman YILMAZ <suleymanymz50@gmail.com>
2025-03-06 15:23:27 +0300

    seed test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('123456789'),
+            'email_verified_at' => now(),
+        ]);
     }
 }
